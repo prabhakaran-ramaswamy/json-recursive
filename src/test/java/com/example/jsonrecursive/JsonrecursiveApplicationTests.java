19103dff9b6d93d08e82d251fbac41cd6c5cceb9
Prabhakaran Ramaswamy <new.prabhakaran@gmail.com>
2023-03-05 19:32:43 +0530

    test class

---
+++ src/test/java/com/example/jsonrecursive/JsonrecursiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonrecursive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonrecursiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
